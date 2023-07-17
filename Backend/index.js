01406492c1da662e68a9023d2eef05c1a525ac68
abhi1506manu <abhi2019manu@gmail.com>
2023-07-17 13:53:10 +0530

    integerated with OpenAIApi

--- Backend/index.js
+++ Backend/index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const cors = require("cors");
 const { connectDB } = require("./mongodb/connect.js");
 
+const postRoutes = require("./routes/postRoutes.js");
+const dalleRoutes = require("./routes/dalleRoutes.js");
+
+
 require("dotenv").config();
 
 //initialize express
@@ -10,6 +14,9 @@ const app = express();
 //additional middleware
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
+
+app.use("/api/v1/post", postRoutes);
+app.use("/api/v1/dalle", dalleRoutes);
 
 app.get("/", async (req, res) => {
   res.send("Hello from server");
